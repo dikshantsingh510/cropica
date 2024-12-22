709e6006e4a32ddd9fecc9a9d396e6619304a5dc
Dikshant Singh <dikshantsingh510@gmail.com>
2024-12-22 11:17:06 +0530

    fixing ui

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>About us</div>
-  )
-}
+    <div className="w-full max-w-7xl min-h-screen max-h-fit flex justify-center flex-col px-5 sm:px-10 xl:px-0 mx-auto font-bricolage_gratesque ">
+      <div className="w-full sm:w-[85%] md:w-[700px] text-center font-light text-3xl md:text-4xl mx-auto">
 
-export default page
+      <h1 className="text-5xl font-bold mb-10 text-center">About us</h1>
+      <p className=" text-2xl text-text_primary">
+        Welcome to Cropica, your trusted partner in tracking crop prices in
+        real-time. Our mission is to empower farmers, traders, and consumers
+        with accurate and up-to-date market information, helping them make
+        informed decisions and stay ahead of market trends.
+      </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import Contact from "../components/Contact";
 
 export default function Home() {
   return (
-    <>
+    <div className="overflow-x-hidden">
       <HeroSection />
       <Marquee
         pauseOnHover
@@ -54,6 +54,6 @@ export default function Home() {
 
       <Faq />
       <Contact />
-    </>
+    </div>
   );
 }
